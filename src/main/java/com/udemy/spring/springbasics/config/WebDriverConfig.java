7f8aa2c83de552382111f2834a51853b3970d272
shankkalra <shankkalra@gmail.com>
2022-03-09 23:15:53 +0530

    Base Class

--- src/main/java/com/udemy/spring/springbasics/config/WebDriverConfig.java
+++ src/main/java/com/udemy/spring/springbasics/config/WebDriverConfig.java
@@ -3,15 +3,25 @@ package com.udemy.spring.springbasics.config;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class WebDriverConfig {
 
+    @Value("${default.timeout:30}")
+    private int timeout;
+
     @Bean
     public WebDriver chromeDriver(){
         WebDriverManager.chromedriver().setup();
         return new ChromeDriver();
+    }
+
+    @Bean
+    public WebDriverWait WebDriverWait(WebDriver driver){
+        return new WebDriverWait(driver,this.timeout);
     }
 }
