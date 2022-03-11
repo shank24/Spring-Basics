904274b23dbb12c8070add2e76a8a3f9701115c4
shankkalra <shankkalra@gmail.com>
2022-03-11 22:39:21 +0530

    Lambda Func

--- src/main/java/com/udemy/spring/springbasics/page/google/SearchComponent.java
+++ src/main/java/com/udemy/spring/springbasics/page/google/SearchComponent.java
@@ -1,6 +1,29 @@
 package com.udemy.spring.springbasics.page.google;
 
 import com.udemy.spring.springbasics.page.Base;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
 
 public class SearchComponent extends Base {
+
+    @FindBy(name="q")
+    private WebElement searchBox;
+
+    @FindBy(name="btnK")
+    private List<WebElement> searchBtns;
+
+    public void search(final String keyword){
+        this.searchBox.sendKeys(keyword);
+        this.searchBtns
+                .stream()
+                .filter(e -> e.isDisplayed() && e.isEnabled())
+                .findFirst()
+                .ifPresent(WebElement::click);
+    }
+    @Override
+    public boolean isAt() {
+        return this.webDriverWait.until((d) -> this.searchBox.isDisplayed() )
+    }
 }
