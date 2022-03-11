d0a17f8045e4daed66cfccdc2a2de5509f6b34b2
shankkalra <shankkalra@gmail.com>
2022-03-11 22:58:41 +0530

    Search Page

---
+++ src/main/java/com/udemy/spring/springbasics/page/google/GooglePage.java
@@ -0,0 +1,13 @@
+package com.udemy.spring.springbasics.page.google;
+
+import com.udemy.spring.springbasics.page.Base;
+
+public class GooglePage extends Base {
+
+
+
+    @Override
+    public boolean isAt() {
+        return false;
+    }
+}
---
+++ src/main/java/com/udemy/spring/springbasics/page/google/SearchResult.java
@@ -0,0 +1,21 @@
+package com.udemy.spring.springbasics.page.google;
+
+import com.udemy.spring.springbasics.page.Base;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
+
+public class SearchResult extends Base {
+
+    @FindBy(css="div.g")
+    private List<WebElement> results;
+
+    public int getCount(){
+        return this.results.size();
+    }
+    @Override
+    public boolean isAt() {
+        return this.webDriverWait.until((d) -> !this.results.isEmpty());
+    }
+}
