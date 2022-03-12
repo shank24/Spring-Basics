20152ad09ca1719d9b8271b360757370dd61159d
shankkalra <shankkalra@gmail.com>
2022-03-12 22:56:59 +0530

    @ConditionalOnMissignBean

--- src/main/java/com/udemy/spring/springbasics/config/WebDriverConfig.java
+++ src/main/java/com/udemy/spring/springbasics/config/WebDriverConfig.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,24 +21,25 @@ public class WebDriverConfig {
     private int timeout;
 
 
-    //@Primary
     @Bean
-    @ConditionalOnProperty(name= "browser", havingValue="chrome")
-    public WebDriver chromeDriver(){
-        WebDriverManager.chromedriver().setup();
-        return new ChromeDriver();
-    }
-
-
-    @Bean
-    @ConditionalOnProperty(name= "browser", havingValue="firefox")
-    public WebDriver firefoxDriver(){
+    @ConditionalOnProperty(name = "browser", havingValue = "firefox")
+    public WebDriver firefoxDriver() {
         WebDriverManager.firefoxdriver().setup();
         return new FirefoxDriver();
     }
 
+    //@Primary
     @Bean
-    public WebDriverWait WebDriverWait(WebDriver driver){
-        return new WebDriverWait(driver,this.timeout);
+    //@ConditionalOnProperty(name= "browser", havingValue="chrome")
+    @ConditionalOnMissingBean
+    //This Missing Bean prop should be at last
+    public WebDriver chromeDriver() {
+        WebDriverManager.chromedriver().setup();
+        return new ChromeDriver();
+    }
+
+    @Bean
+    public WebDriverWait WebDriverWait(WebDriver driver) {
+        return new WebDriverWait(driver, this.timeout);
     }
 }
