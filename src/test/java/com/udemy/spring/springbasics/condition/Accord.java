8f6223b182eef0e55d035d040709f260decf4fd2
shankkalra <shankkalra@gmail.com>
2022-03-15 21:59:36 +0530

    @Qualifier

--- src/test/java/com/udemy/spring/springbasics/condition/Accord.java
+++ src/test/java/com/udemy/spring/springbasics/condition/Accord.java
@@ -1,10 +1,12 @@
 package com.udemy.spring.springbasics.condition;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConditionalOnExpression("${car.speed} >= 70")
+//@ConditionalOnExpression("${car.speed} >= 70")
+@Qualifier("accord")
 public class Accord implements Car {
 
     @Override
--- src/test/java/com/udemy/spring/springbasics/condition/CarTest.java
+++ src/test/java/com/udemy/spring/springbasics/condition/CarTest.java
@@ -1,10 +1,12 @@
 package com.udemy.spring.springbasics.condition;
 import com.udemy.spring.springbasics.SpringBaseTestNGTest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.testng.annotations.Test;
 
 public class CarTest extends SpringBaseTestNGTest {
 
+    @Qualifier("accord")
     @Autowired
     private Car car;
 
