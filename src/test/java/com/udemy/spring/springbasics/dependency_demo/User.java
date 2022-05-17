e08fe94d7908a3ba7c3d4fea4657579e22b6af2b
shankkalra <shankkalra@gmail.com>
2022-05-17 17:48:14 +0530

    Dependency Injection With Setter Approach

--- src/test/java/com/udemy/spring/springbasics/dependency_demo/User.java
+++ src/test/java/com/udemy/spring/springbasics/dependency_demo/User.java
@@ -6,19 +6,20 @@ import org.springframework.stereotype.Component;
 @Component
 public class User {
 
+    @Autowired
     private Address address;
+
+    @Autowired
     private Salary salary;
 
     //Setter Approach
-    @Autowired
-    public void setAddress(Address address) {
+    /*public void setAddress(Address address) {
         this.address = address;
     }
 
-    @Autowired
     public void setSalary(Salary salary) {
         this.salary = salary;
-    }
+    }*/
 
     /*public User(Address address, Salary salary) {
         this.address = address;
