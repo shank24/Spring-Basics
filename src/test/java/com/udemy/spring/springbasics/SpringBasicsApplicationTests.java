1284b0739a5b713c658196338a8611062f7f1d12
shankkalra <shankkalra@gmail.com>
2022-05-23 22:25:39 +0530

    Adding Dependency

--- src/test/java/com/udemy/spring/springbasics/SpringBasicsApplicationTests.java
+++ src/test/java/com/udemy/spring/springbasics/SpringBasicsApplicationTests.java
@@ -1,25 +1,37 @@
 package com.udemy.spring.springbasics;
 
-import com.github.javafaker.Faker;
-import com.udemy.spring.springbasics.dependency_demo.User;
+import com.udemy.spring.springbasics.bean.lifecycle.Television;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.List;
 
 @SpringBootTest
 class SpringBasicsApplicationTests {
 
-	
+	@Autowired
+	private Television tv;
+
+	@Test
+	void contextLoads() {
+
+		this.tv.playMovie();
+
+		/*user.printDetails();
+		System.out.println(faker.name().firstName());*/
+
+	}
+
+
+	// Objects
+
+	/*
 	@Autowired
 	private User user;
 
 	@Autowired
 	private Faker faker;
 
-	/*@Value("${TEST_URL:www.example.com}")
+	@Value("${TEST_URL:www.example.com}")
 	private String url;
 
 	@Value("${fruits}")
@@ -27,27 +39,21 @@ class SpringBasicsApplicationTests {
 
 	@Value("${PATH}")
 	private String path;
-*/
-	@Test
-	void contextLoads() {
+	*/
 
-		user.printDetails();
-		System.out.println(faker.name().firstName());
-
-	}
 
 
 	/*
 		System.out.println(this.fruit);
 		System.out.println(this.url);
-		*/
+	*/
 
 	/*
 	Address address = new Address();
 	Salary salary = new Salary();
 	User user = new User(address,salary);
 	user.printDetails();
-	 */
+	*/
 }
 
 
