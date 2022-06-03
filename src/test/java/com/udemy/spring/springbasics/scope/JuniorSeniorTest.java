089530068faf8a3a59999a662cd2bba473558d60
shankkalra <shankkalra@gmail.com>
2022-06-03 22:05:19 +0530

    Bean Scope

---
+++ src/test/java/com/udemy/spring/springbasics/scope/JuniorSeniorTest.java
@@ -0,0 +1,25 @@
+package com.udemy.spring.springbasics.scope;
+
+import com.udemy.spring.springbasics.SpringBaseTestNGTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.testng.annotations.Test;
+
+public class JuniorSeniorTest extends SpringBaseTestNGTest {
+
+    @Autowired
+    private JuniorEng juniorEng;
+
+    @Autowired
+    private SeniorEng seniorEng;
+
+    @Test
+    public void scopeTest(){
+        juniorEng.setAmount(100);
+        System.out.println("Junior :: " + this.juniorEng.getSalary().getAmount());
+
+        seniorEng.setAmount(500);
+        System.out.println("Senior :: " + this.seniorEng.getSalary().getAmount());
+
+
+    }
+}
