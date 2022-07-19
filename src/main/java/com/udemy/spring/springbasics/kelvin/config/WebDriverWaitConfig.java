d2291aaa846f4ed391b73b7b7807704f9a688800
shankkalra <shankkalra@gmail.com>
2022-07-19 18:14:09 +0530

    Scope Addition

--- src/main/java/com/udemy/spring/springbasics/kelvin/config/WebDriverWaitConfig.java
+++ src/main/java/com/udemy/spring/springbasics/kelvin/config/WebDriverWaitConfig.java
@@ -4,7 +4,9 @@ import com.udemy.spring.springbasics.kelvin.annotation.LazyConfiguration;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
 
 @LazyConfiguration
 public class WebDriverWaitConfig {
@@ -13,6 +15,7 @@ public class WebDriverWaitConfig {
     private int timeOut;
 
     @Bean
+    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
     public WebDriverWait webDriverWait(WebDriver driver)
     {
         return new WebDriverWait(driver,this.timeOut);
